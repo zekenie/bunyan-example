9aee48f8850b1a75241f82f7f6e0e51299ba344c
Zeke Nierenberg <hampzan09@gmail.com>
2018-07-19 10:20:18 -0500

    bunyan added

--- api/middleware.js
+++ api/middleware.js
@@ -1,5 +1,14 @@
+const log = require("../log");
 const bodyParser = require("body-parser");
 
 module.exports = ({ app }) => {
   app.use(bodyParser.json());
+
+  app.use((req, res, next) => {
+    log.info({ req }, "Request");
+    res.on("finish", () => {
+      log.info({ res }, "response");
+    });
+    next();
+  });
 };
--- processes/worker.js
+++ processes/worker.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const log = require("../log");
 
 const { Pages, Scrapes } = require("../data");
 const makeScraper = require("../scrape");
@@ -10,14 +11,15 @@ async function main() {
     const pageAPI = await scraper(page.uri);
     const { html, hash } = await pageAPI.html();
     const screenshot = await pageAPI.screenshot();
-    console.log(
-      "html",
-      html.slice(0, 40),
-      " {...} ",
-      html.slice(html.length - 40)
-    );
-    console.log("hash", hash);
-    console.log("screenshot", screenshot);
+    log.debug({ url: page.url, hash, binarySize: screenshot.length }, "scrape");
+    // console.log(
+    //   "html",
+    //   html.slice(0, 40),
+    //   " {...} ",
+    //   html.slice(html.length - 40)
+    // );
+    // console.log("hash", hash);
+    // console.log("screenshot", screenshot);
     await Scrapes.create({
       page_id: page.id,
       html,
@@ -26,14 +28,15 @@ async function main() {
     });
     await pageAPI.close();
   }
+  return pages.length;
 }
 
 main()
-  .then(() => {
-    console.log("done");
+  .then(numPages => {
+    log.info({ numPages }, "done with scraping");
     process.exit(0);
   })
   .catch(err => {
-    console.error(err);
+    log.error({ err }, "scraper error");
     process.exit(1);
   });
